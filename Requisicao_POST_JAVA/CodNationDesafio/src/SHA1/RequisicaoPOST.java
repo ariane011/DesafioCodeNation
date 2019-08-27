2a44696e1ec5d637352973a1cab74e98e7fef28a
Ariane Sousa <ariane.sousa011@gmail.com>
2019-08-26 23:14:49 -0300

    ...

--- Requisicao_POST_JAVA/CodNationDesafio/src/SHA1/RequisicaoPOST.java
+++ Requisicao_POST_JAVA/CodNationDesafio/src/SHA1/RequisicaoPOST.java
@@ -1,38 +1,55 @@
 package SHA1;
 
+
 import java.io.IOException;
-import java.io.PrintStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class RequisicaoPOST {
 	
-	public static String post(String json) throws IOException {
-
-		URL url = new URL("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=6cd0100af830f8244ed55302a1950ab14354e5af");
-
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-		connection.setRequestMethod("POST");
-
-		connection.setRequestProperty("Content-type", "application/json"); connection.setRequestProperty("Accept", "application/json");
-
-		connection.setDoOutput(true);
-
-		PrintStream printStream = new PrintStream(connection.getOutputStream()); printStream.println(json);
-
-		connection.connect();
-
-		String jsonDeResposta = new Scanner(connection.getInputStream()).next();
-
-		return jsonDeResposta; 
-		
-	}
-	
 	public static void main(String[] args) throws IOException {
-		post("{ \"numero_casas\":2, \"token\":\"6cd0100af830f8244ed55302a1950ab14354e5af\", \"cifrado\":\"rgqrng yknn tgcnkbg vjcv uqhvyctg ku pqv c rtqfwev; aqw wug kv vq dwknf c rtqfwev. nkpwu vqtxcnfu\", \"decifrado\":\"tistpi ampp viepmdi xlex wsjxaevi mw rsx e tvshygx; csy ywi mx xs fymph e tvshygx. pmryw xsvzephw\", \"resumo_criptografico\":\"997DBF7EBC77419A9F953C33CBEB49460D381486\" }\r\n" + 
-				"");
-	}
+        postUser();
+    }
+
+    public static void postUser() throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=6cd0100af830f8244ed55302a1950ab14354e5af");
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "multipart/form-data");
+            String json = "{\r\n" +
+                    "  \"numero_casas\": \"2\",\r\n" +
+                    "  \"token\": \"6cd0100af830f8244ed55302a1950ab14354e5af\",\r\n" +
+                    "  \"cifrado\": \"rgqrng yknn tgcnkbg vjcv uqhvyctg ku pqv c rtqfwev; aqw wug kv vq dwknf c rtqfwev. nkpwu vqtxcnfu\",\r\n" +
+                    "  \"decifrado\": \"tistpi ampp viepmdi xlex wsjxaevi mw rsx e tvshygx; csy ywi mx xs fymph e tvshygx. pmryw xsvzephw\",\r\n" +
+                    "  \"resumo_criptografico\": \"997DBF7EBC77419A9F953C33CBEB49460D381486\",\r\n" +
+                   "}";
+                        
+            StringEntity stringEntity = new StringEntity(json);
+            httpPost.setEntity(stringEntity);
+
+            System.out.println("Executing request " + httpPost.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler < String > responseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+            String responseBody = httpclient.execute(httpPost, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+        }
+    }
   
 }
